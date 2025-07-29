ef07eacbaec3db340cc0bd3065ab7c21c6d53d40
semajh <jamesharrison2262@gmail.com>
2025-07-29 18:14:43 +0100

    added main

---
+++ Source.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+	// Declare a variable of type int
+	int number = 42;
+	// Output the value of the variable to the console
+	std::cout << "The value of number is: " << number << std::endl;
+	return 0;
+}
