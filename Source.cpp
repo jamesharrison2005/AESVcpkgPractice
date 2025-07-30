4235d32ffc3bcfc4c16ab50e40d58736714b6d00
semajh <jamesharrison2262@gmail.com>
2025-07-30 17:29:00 +0100

    added more comments and install instructions

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 #include "AESEncryption.h"
 
+/*
+How to Run:
+1. In powershell run the following command(if you dont have vcpkg installed already): vcpkg integrate install 
+2.go to Project Properties -> vcpkg -> Use Vcpkg Manifest -> Yes
+3.Build the solution which should download the Crypto++ library and link it to your project
+4.fi the build fails you may need to install vcpkg.exe manually (also check vcpkg.json has the dependency cryptopp)
+5.Run the Program
+6.If that doesnt work im sorry i dont know how to use vcpkg properly
+*/
+
 std::string EnterText();
 void Menu();
 
@@ -10,7 +20,7 @@ int main()
 
 	return 0;
 }
-
+// Function to enter text from the user
 std::string EnterText()
 {
 	std::string text;
@@ -24,7 +34,7 @@ std::string EnterText()
 
 	return text;
 }
-
+// Function to display the menu and handle user input
 void Menu()
 {
 	std::cout << "Welcome to the AES Encryption Test" << std::endl;
